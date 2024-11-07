f4004ee64affe28639e41b90eaeda5f0b96dab8d
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-11-07 10:03:47 +0530

    it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
+import Loader from "./components/Loader";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App2 = () => {
   const vidRef = useRef(null);
   const canvasRef = useRef(null);
   const [prediction, setPrediction] = useState([]);
   const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
@@ -30,10 +34,42 @@ const App2 = () => {
     setImage(base64Image);
   };
 
+  useEffect(() => {
+    if (image) {
+      setLoading(true);
+      axios({
+        method: "POST",
+        url: "https://detect.roboflow.com/plastic-detection-szrn0/1",
+        params: {
+          api_key: import.meta.env.VITE_API_KEY
+        },
+        data: image,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+        .then(function (response) {
+          const { predictions } = response.data;
+          setPrediction(predictions);
+          toast.success("Image analysis completed!");
+        })
+        .catch(function (error) {
+          console.error(error);
+          toast.error(error.message);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [image]);
+
   return (
     <div className="bg-gray-800 flex justify-center items-center h-screen">
+      <ToastContainer />
+      {loading && <Loader />}
       <div className="relative flex flex-col gap-2">
-        {image && <img src={image} alt="Captured image" />}
+        <h1 className="text-white text-3xl self-center font-semibold">Plastic Detection</h1>
+        {image && <img src={image} alt="Captured image" className="rounded" />}
         {prediction &&
           prediction.map((item, index) => {
             const top = item.y - item.height / 2;
@@ -47,11 +83,11 @@ const App2 = () => {
                 className="border-red-500 border-2 absolute"
                 style={{ top, left, width: item.width, height: item.height }}
               >
-                {(prediction[0].confidence * 100).toFixed(2)}% | {item.class}
+                <span className="bg-gray-200">{(prediction[0].confidence * 100).toFixed(2)}% | {item.class}</span>
               </div>
             );
           })}
-        {!image && <video ref={vidRef}></video>}
+        {!image && <video ref={vidRef} className="rounded"></video>}
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={captureImage}
